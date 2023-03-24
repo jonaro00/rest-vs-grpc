baf4c3cd9178fe115708636c2408165c282a656b
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2023-03-24 17:41:22 +0100

    Implement hello world rest rust

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,28 +1,80 @@
-use proto::inventory::{greeting_service_client::GreetingServiceClient, HelloRequest};
-use tonic::Request;
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = GreetingServiceClient::connect("http://127.0.0.1:1337").await?;
+    let mut args = std::env::args().skip(1);
+    let mode = args.next();
 
-    let arr = &mut [0i32; 10];
-    let start = std::time::Instant::now();
-    loop {
-        let req = Request::new(HelloRequest {
-            name: "Hasbulla".into(),
-        });
-        // println!("Sending {:?}", req);
-        let _res = client.greet(req).await?;
-        // println!("Got response {:?}", res);
-        let now = std::time::Instant::now();
-        let diff = now.duration_since(start).as_secs() as usize;
-        if diff >= arr.len() {
-            break;
-        }
-        arr[diff] += 1;
-    }
+    let addr = "http://127.0.0.1:1337";
+
+    let arr = if mode == Some("rest".into()) {
+        rest::run(addr).await?
+    } else if mode == Some("grpc".into()) {
+        grpc::run(addr).await?
+    } else {
+        panic!("provide rest or grpc")
+    };
 
     println!("{arr:?}");
 
     Ok(())
+}
+
+mod rest {
+    use reqwest::Client;
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Debug, Serialize, Deserialize)]
+    struct HelloRequest {
+        name: String,
+    }
+    #[derive(Debug, Serialize, Deserialize)]
+    struct HelloResponse {
+        greeting: String,
+    }
+
+    pub async fn run(addr: &'static str) -> Result<[i32; 10], Box<dyn std::error::Error>> {
+        let client = Client::builder().http1_only().build()?;
+        let arr = &mut [0i32; 10];
+        let start = std::time::Instant::now();
+        loop {
+            let req = HelloRequest {
+                name: "Hasbullah".into(),
+            };
+            // println!("Sending {:?}", req);
+            let _res = client.post(addr).json(&req).send().await?;
+            // println!("Got response {:?}", res);
+            let now = std::time::Instant::now();
+            let diff = now.duration_since(start).as_secs() as usize;
+            if diff >= arr.len() {
+                break;
+            }
+            arr[diff] += 1;
+        }
+        Ok(*arr)
+    }
+}
+
+mod grpc {
+    use proto::inventory::{greeting_service_client::GreetingServiceClient, HelloRequest};
+    use tonic::Request;
+
+    pub async fn run(addr: &'static str) -> Result<[i32; 10], Box<dyn std::error::Error>> {
+        let mut client = GreetingServiceClient::connect(addr).await?;
+        let arr = &mut [0i32; 10];
+        let start = std::time::Instant::now();
+        loop {
+            let req = Request::new(HelloRequest {
+                name: "Hasbulla".into(),
+            });
+            // println!("Sending {:?}", req);
+            let _res = client.greet(req).await?;
+            // println!("Got response {:?}", res);
+            let now = std::time::Instant::now();
+            let diff = now.duration_since(start).as_secs() as usize;
+            if diff >= arr.len() {
+                break;
+            }
+            arr[diff] += 1;
+        }
+        Ok(*arr)
+    }
 }
--- server-rust/src/main.rs
+++ server-rust/src/main.rs
@@ -1,34 +1,82 @@
-use proto::inventory::{
-    greeting_service_server::{GreetingService, GreetingServiceServer},
-    HelloRequest, HelloResponse,
-};
-use tonic::{transport::Server, Request, Response, Status};
-
-struct GreetingServer {}
-
-#[tonic::async_trait]
-impl GreetingService for GreetingServer {
-    async fn greet(
-        &self,
-        request: Request<HelloRequest>,
-    ) -> Result<Response<HelloResponse>, Status> {
-        let req = request.into_inner();
-        let res = HelloResponse {
-            greeting: format!("Hello {}!", req.name),
-        };
-        Ok(Response::new(res))
-    }
-}
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "127.0.0.1:1337".parse()?;
-    let server = GreetingServer {};
+    let mut args = std::env::args().skip(1);
+    let mode = args.next();
+    let port = args.next().unwrap_or("1337".into()).parse()?;
 
-    Server::builder()
-        .add_service(GreetingServiceServer::new(server))
-        .serve(addr)
-        .await?;
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), port);
 
-    Ok(())
+    if mode == Some("rest".into()) {
+        rest::run(addr).await
+    } else if mode == Some("grpc".into()) {
+        grpc::run(addr).await
+    } else {
+        panic!("provide rest or grpc")
+    }
+}
+
+mod rest {
+    use std::net::SocketAddr;
+
+    use axum::{routing::post, Json, Router, Server};
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Serialize, Deserialize)]
+    struct HelloRequest {
+        name: String,
+    }
+    #[derive(Serialize, Deserialize)]
+    struct HelloResponse {
+        greeting: String,
+    }
+
+    async fn hello(Json(req): Json<HelloRequest>) -> Json<HelloResponse> {
+        Json(HelloResponse {
+            greeting: format!("Hello {}!", req.name),
+        })
+    }
+
+    pub async fn run(addr: SocketAddr) -> Result<(), Box<dyn std::error::Error>> {
+        Server::bind(&addr)
+            .serve(Router::new().route("/", post(hello)).into_make_service())
+            .await?;
+        Ok(())
+    }
+}
+
+mod grpc {
+    use std::net::SocketAddr;
+
+    use proto::inventory::{
+        greeting_service_server::{GreetingService, GreetingServiceServer},
+        HelloRequest, HelloResponse,
+    };
+    use tonic::{transport::Server, Request, Response, Status};
+
+    struct GreetingServer;
+
+    #[tonic::async_trait]
+    impl GreetingService for GreetingServer {
+        async fn greet(
+            &self,
+            request: Request<HelloRequest>,
+        ) -> Result<Response<HelloResponse>, Status> {
+            let req = request.into_inner();
+            let res = HelloResponse {
+                greeting: format!("Hello {}!", req.name),
+            };
+            Ok(Response::new(res))
+        }
+    }
+
+    pub async fn run(addr: SocketAddr) -> Result<(), Box<dyn std::error::Error>> {
+        let server = GreetingServer;
+        Server::builder()
+            .add_service(GreetingServiceServer::new(server))
+            .serve(addr)
+            .await?;
+        Ok(())
+    }
 }
